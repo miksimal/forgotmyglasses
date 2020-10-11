4d4e194a5fc342c263b03cd2ddeff975b5939ba4
Mikkel Lauritzen <mikkel.hlauritzen@gmail.com>
2020-10-11 19:21:21 +0100

    Updates

---
+++ precache-manifest.1d97beab310327221cfd351586b25e37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b8a4a5dc37d0e826db20e6ddc26ba1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb413ef8872535860a7e",
+    "url": "/static/css/main.7dc77a52.chunk.css"
+  },
+  {
+    "revision": "7c787986b15395c450e6",
+    "url": "/static/js/2.441bd46e.chunk.js"
+  },
+  {
+    "revision": "21c75a98ff1b0cc08c27fd17e11b8c68",
+    "url": "/static/js/2.441bd46e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb413ef8872535860a7e",
+    "url": "/static/js/main.21eed841.chunk.js"
+  },
+  {
+    "revision": "788aaaf59f2368879544",
+    "url": "/static/js/runtime-main.a9755233.js"
+  }
+]);
